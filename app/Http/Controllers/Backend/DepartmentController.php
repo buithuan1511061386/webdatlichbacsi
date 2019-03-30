523e746b5c8d76b4b320e911bfbd7cfe4674ff3e
Nguyen Tran <tapunyn95@gmail.com>
2019-03-30 21:13:27 +0700

    cap nhat controller Department

--- app/Http/Controllers/Backend/DepartmentController.php
+++ app/Http/Controllers/Backend/DepartmentController.php
@@ -23,7 +23,94 @@ class DepartmentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-	 
-	 
+	  public function create()
+    {
+        return view('backend.department.adddepartment');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Department;
+        $data->name = $request->name;
+        $data->save();
+         return redirect('admin/department')->with('status','Thêm thành công');;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Khong vao dc route nay
+        // dd($id);
+        $data = Department::find($id);
+        //Để load khóa ngoại
+        //Ví dụ
+        //$data = Hospital::find($id);
+        //$region = Region::where('id',$data->id_region)->get();
+        //$data sẽ ra chi tiết bệnh viện đó, còn $region sẽ ra bệnh viện đó có miền gì
+        //compact cả 2 return view('...',compact($data,$region));
+        //khi do region bên view sẽ vào vòng lặp foreach in ra :D, nó sẽ load hết miền có id trùng với id_region của bệnh viện, có cả tên. có gì tui làm thử ùi tui hỏi ông. tks nhaok:D
+        return view('backend.department.editdepartment',compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, $id)
+    // {
+    //     // dd($request->all())
+    //   $data = Region::find($id);
+    //   $data->name = $request->get('name');
+    //   $data->save();
+
+    //   return redirect('admin/region');
+    // }
+    public function update_post(Request $req, $id)
+    {
+        // dd($req->all());
+        $data = Department::find($id);
+        $data->name = $req->name;
+        $data->save();
+        return redirect('admin/department')->with('status','Sửa thành công');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Department::find($id);
+     $data->delete();
+
+     return redirect('admin/department')->with('status','Xóa thành công');
+    } 
 }
+
 ?>
