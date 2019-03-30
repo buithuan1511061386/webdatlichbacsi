811d8e1593800e1b725da95155146e5af9af5a6f
Nguyen Tran <tapunyn95@gmail.com>
2019-03-30 21:30:29 +0700

    update controller Doctortime

--- app/Http/Controllers/Backend/DoctortimeController.php
+++ app/Http/Controllers/Backend/DoctortimeController.php
@@ -68,6 +68,67 @@ class DoctortimeController extends Controller
         // $department = Department::all();
         // return view('backend.user.view_user',compact('data','region','hospital','department'));
     }
+	/**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Khong vao dc route nay
+        // dd($id);
+        $data = Doctortime::find($id);
+        // $region = Region::all();
+        // $hospital = Hospital::all();
+        // $department = Department::all();
+        //Để load khóa ngoại
+        //Ví dụ
+        //$data = Hospital::find($id);
+        //$region = Region::where('id',$data->id_region)->get();
+        //$data sẽ ra chi tiết bệnh viện đó, còn $region sẽ ra bệnh viện đó có miền gì
+        //compact cả 2 return view('...',compact($data,$region));
+        //khi do region bên view sẽ vào vòng lặp foreach in ra :D, nó sẽ load hết miền có id trùng với id_region của bệnh viện, có cả tên. có gì tui làm thử ùi tui hỏi ông. tks nhaok:D
+        return view('backend.doctortime.editdoctortime',compact('data'));
+    }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, $id)
+    // {
+    //     // dd($request->all())
+    //   $data = Region::find($id);
+    //   $data->name = $request->get('name');
+    //   $data->save();
+
+    //   return redirect('admin/region');
+    // }
+    public function update_post(Request $req, $id)
+    {
+        // dd($req->all());
+        $data = Doctortime::find($id);
+         $data->date = $req->date;
+        $data->time_start = $req->time_start;
+        $data->time_end = $req->time_end;
+        $data->save();
+        return redirect('admin/doctortime/list')->with('status','Sửa thành công');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Doctortime::find($id);
+     $data->delete();
+     return redirect('admin/doctortime/list')->with('status','Xóa thành công');
+    }
 
 ?>
