645b29396673b06e6e374a623b92471a1cdbd9fd
Nguyen Tran <tapunyn95@gmail.com>
2019-03-30 16:50:04 +0700

    file controler region

---
+++ app/Http/Controllers/Backend/RegionController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Region;
+class RegionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Region::all();
+        return view('backend.region.region_list',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backend.region.addregion');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Region;
+        $data->name = $request->name;
+        $data->save();
+         return redirect('admin/region')->with('status','Thêm thành công');;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Khong vao dc route nay
+        // dd($id);
+        $data = Region::find($id);
+        //Để load khóa ngoại
+        //Ví dụ
+        //$data = Hospital::find($id);
+        //$region = Region::where('id',$data->id_region)->get();
+        //$data sẽ ra chi tiết bệnh viện đó, còn $region sẽ ra bệnh viện đó có miền gì
+        //compact cả 2 return view('...',compact($data,$region));
+        //khi do region bên view sẽ vào vòng lặp foreach in ra :D, nó sẽ load hết miền có id trùng với id_region của bệnh viện, có cả tên. có gì tui làm thử ùi tui hỏi ông. tks nhaok:D
+        return view('backend.region.editregion',compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, $id)
+    // {
+    //     // dd($request->all())
+    //   $data = Region::find($id);
+    //   $data->name = $request->get('name');
+    //   $data->save();
+
+    //   return redirect('admin/region');
+    // }
+    public function update_post(Request $req, $id)
+    {
+        // dd($req->all());
+        $data = Region::find($id);
+        $data->name = $req->name;
+        $data->save();
+        return redirect('admin/region')->with('status','Sửa thành công');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Region::find($id);
+     $data->delete();
+
+     return redirect('admin/region')->with('status','Xóa thành công');
+    }
+}
