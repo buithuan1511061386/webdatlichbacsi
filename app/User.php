5ea3b58c4e2b13e34d4e2641723764a49d4acbd1
tanphat0009 <34399328+tanphat0009@users.noreply.github.com>
2019-03-31 11:34:45 +0700

    fix loi

--- app/User.php
+++ app/User.php
@@ -36,4 +36,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+	 public function region()
+    {
+        return $this->belongsTo('App\Region','region_id','id');
+    }
+        public function department()
+    {
+        return $this->belongsTo('App\Department','department_id','id');
+    }
+        public function hospital()
+    {
+        return $this->belongsTo('App\Hospital','hospital_id','id');
+    }
 }
