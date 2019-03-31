3422f0cd8ac4b2fa9d148e238add75064c3df5e0
Thuận Bùi <buithuan1511061386@gmail.com>
2019-03-31 14:27:39 +0700

    chuc nang xem bac si

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends Controller
         $typeofnews = Typeofnews::all();
         return view('frontend.home.news',compact('region','hospital','department','typeofnews'));
     }
-	
+
 	public function regionlist(Request $request)
     {
         $region= Region::all();
@@ -76,7 +76,7 @@ class HomeController extends Controller
         $typeofnews= Typeofnews::all();
         return view('frontend.home.regionlist',compact('region','hospital','department','user','request', 'typeofnews'));
     }
-	
+
 	public function hospitallist(Request $request)
     {
         $region= Region::all();
@@ -97,6 +97,49 @@ class HomeController extends Controller
 
         return view('frontend.home.departmentlist',compact('region','hospital','department','user','request','typeofnews'));
     }
+    public function show($id)
+    {
+        //Lấy dữ kiện
+        $data = User::find($id);
+        $region = Region::all();
+        $hospital = Hospital::all();
+        $department = Department::all();
+        $appointment = Appointment::where('doctor_id',$id)->get();
+        $doctortime =Doctortime::where('user_id',$id)->get();
+        $user = User::where('user_level_id',2)->get();
+        $typeofnews= Typeofnews::all();
+        //Lấy ngày giờ bác sĩ
+        $array = array();
+        foreach($doctortime as $item) {
+            // $begin =new DateTime($item->time_start);
+            $date = date('Y-m-d',strtotime("$item->date"));
+            $begin = DateTime::createFromFormat('Y-m-d H:i:s', "$date $item->time_start");
+            $end = DateTime::createFromFormat('Y-m-d H:i:s', "$date $item->time_end");
+            $interval = DateInterval::createFromDateString('30 min');
+            $timesDate = new DatePeriod($begin, $interval, $end);
+            array_push($array,$timesDate);
+        }
+        $appointments = array();
+        if($appointment != null){
+            foreach($appointment as $day){
+                $getDay = date('Y-m-d', strtotime("$day->date"));
+                $datetime = date('Y-m-d H:i:s', strtotime("$getDay $day->time"));
+                array_push($appointments,$datetime);
+            }
+        }
+        $doctor = array();
+        foreach($array as $timesDate){
+            foreach($timesDate as $value){
+                $item = $value->format('Y-m-d H:i:s');
+                array_push($doctor,$item);
+            }
+        }
+        $diff1 = array_diff($appointments,$doctor);
+        $diff2 = array_diff($doctor,$appointments);
+        $times = array_merge($diff1,$diff2);
+        return view('frontend.home.doctordetail',compact('data','region','hospital','department','user','doctortime', 'typeofnews','times'));
+    }
+
 }
 
 ?>
